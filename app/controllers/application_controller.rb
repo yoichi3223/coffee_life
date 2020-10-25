95a804e6c339d8cb16eb7771f162d9f88919c946
yoichi3223 <yoichi3223@gmail.com>
2020-10-25 10:28:46 +0000

    devise add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#show'
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
