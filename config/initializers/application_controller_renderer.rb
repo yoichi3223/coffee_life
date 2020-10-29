1dd7abe1d566d30a153a75b6b09a75fa524fbbe3
yoichi3223 <yoichi3223@gmail.com>
2020-10-29 10:38:09 +0000

    refile(user) add

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '98d235e4cae5b116d14a1e30cac25e2b79ccf7b1abf6d84a5dbf7c90a021f29168ff78d1bd07945c5c602b235de7d9c759d92f165df25e38a9f6c3a0310ba73b'
