0ddb4be5f419e8e29a56a2c50a24591f0a5bafc8
yoichi3223 <yoichi3223@gmail.com>
2020-11-10 12:09:47 +0000

    columchange drinks:user_id

---
+++ db/migrate/20201110115628_change_datatype_user_id_of_drinks.rb
@@ -0,0 +1,10 @@
+class ChangeDatatypeUserIdOfDrinks < ActiveRecord::Migration[5.2]
+
+  def up
+    change_column :drinks, :user_id, :bigint
+  end
+
+  def down
+    change_column :drinks, :user_id, :integer
+  end
+end
